682f2ae28d32a270ecfb321d4faef321ef19d0e0
Aleksej <Aleksejmocny@gmail.com>
2022-05-22 21:39:54 +0200

    feat:  04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.promises.readdir(path.join(__dirname, 'files')).then( response => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
+  response.forEach ((file)=> {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {});
+  });
+});
