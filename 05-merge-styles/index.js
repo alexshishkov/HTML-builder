d6e3b60bd49c676930682db2473d6a6a3386e4a7
Aleksej <Aleksejmocny@gmail.com>
2022-05-24 16:47:34 +0200

    fix: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 fs.promises.readdir(path.join(__dirname, 'styles')).then( response => { response.forEach((file) => {
   if(file.substring(file.indexOf('.')) === '.css') {
+    fs.unlink(path.join((path.join(__dirname, 'project-dist', 'bundle.css'))), () => {});
     fs.createReadStream(path.join(__dirname, 'styles', file)).pipe(
       fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), { flags: 'a'})
     );
