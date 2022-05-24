be73df22e6c522f06e1c8c6bd23af7bd2c330b62
Aleksej <Aleksejmocny@gmail.com>
2022-05-24 16:37:40 +0200

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 fs.promises.readdir(path.join(__dirname, 'files')).then( response => {
   fs.mkdir(path.join(__dirname, 'files-copy'), () => {});
-  response.forEach ((file)=> {
-    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {});
+  fs.promises.readdir(path.join(__dirname, 'files-copy')).then( res => {
+    res.forEach ((file)=> {
+      fs.unlink(path.join((path.join(__dirname, 'files-copy')), file), () => {});
+    });
+    response.forEach ((file)=> {
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => {});
+    });
   });
 });
